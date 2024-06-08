188b681669ab714a9b3e6db0b50792416372157a
Sultan Mukhtarov <suli419956@gmail.com>
2024-06-08 18:27:55 +0600

    Initial Commit.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import BackDrop from './components/BackDrop/BackDrop';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+
+  return (
+    <>
+      <div>
+        <BackDrop />
+      </div>
+
+    </>
+  );
+};
+
+export default App
