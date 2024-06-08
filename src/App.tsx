16e60753bf8d9edbe81bb727d22043002b7240e9
Sultan Mukhtarov <suli419956@gmail.com>
2024-06-08 19:47:53 +0600

    evry logic of Alert was moved to own component file. So in app , it will look more clear.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import BackDrop from './components/BackDrop/BackDrop';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react';
-import Alert from './components/Alert/Alert';
-import {AlertColor} from './types';
+import ShowAlert from './components/ShowAlert';
 
 const App = () => {
-
-  const [showAlert, setShowAlert] = useState(true);
-
-  const closeAlert = () => {
-    setShowAlert(false)
-  }
 
   return (
     <>
       <div>
         <BackDrop />
+        <ShowAlert />
       </div>
-      <div>
-        <div className="container mt-5">
-          {showAlert && (
-            <Alert color={AlertColor.primary} onDismiss={closeAlert}>
-              This is a warning type alert
-            </Alert>
-          )}
-          <Alert color={AlertColor.success}>This is a success type alert</Alert>
-        </div>
-      </div>
-
     </>
   );
 };
---
+++ src/components/ShowAlert.tsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+import Alert from './Alert/Alert';
+import {AlertColor} from '../types';
+
+const ShowAlert = () => {
+
+  const [showAlert, setShowAlert] = useState(true);
+
+  const closeAlert = () => {
+    setShowAlert(false)
+  }
+
+  return (
+    <div>
+      <div className="container mt-5">
+        {showAlert && (
+          <Alert color={AlertColor.primary} onDismiss={closeAlert}>
+            This is a warning type alert
+          </Alert>
+        )}
+        <Alert color={AlertColor.success}>This is a success type alert</Alert>
+      </div>
+    </div>
+  );
+};
+
+export default ShowAlert;
